ba7addd313251f07595b3cc8f70c087d9fedabc6
Wramberg <jesper.wramberg@gmail.com>
2017-05-31 17:18:04 +0200

    Add complete support for colors. Note some cleaning is required from this commit. Performance may need optimization.

--- SublimeTerminalBuffer.py
+++ SublimeTerminalBuffer.py
@@ -1,3 +1,5 @@
+import time
+
 import sublime
 import sublime_plugin
 
@@ -29,6 +31,9 @@ class SublimeTerminalBuffer():
         sublime_view.settings().set("terminal_view", True)
         sublime.active_window().focus_view(sublime_view)
 
+        # Initialize private settings for coloring
+        sublime_view.settings().set("terminal_view_color_regions", {})
+
         # Start out with a large terminal to ensure ST draws scrollbars etc. If
         # we do not do this we will end up resizing terminal first time its
         # rendered
@@ -44,7 +49,7 @@ class SublimeTerminalBuffer():
 
     def update_view(self):
         if len(self._screen.dirty) > 0:
-            color_map = convert_pyte_buffer_to_colormap_for_lines(self._screen.buffer, self._screen.dirty)
+            color_map = convert_pyte_buffer_lines_to_colormap(self._screen.buffer, self._screen.dirty)
             update = {
                 "lines": list(self._screen.dirty),
                 "display": self._screen.display,
@@ -94,7 +99,8 @@ class SublimeTerminalBuffer():
     def _update_cursor(self):
         cursor = self._screen.cursor
         if cursor:
-            self._view.run_command("terminal_view_move_cursor", {"cursor_x": cursor.x, "cursor_y": cursor.y})
+            update = {"cursor_x": cursor.x, "cursor_y": cursor.y}
+            self._view.run_command("terminal_view_move_cursor", update)
 
 
 class TerminalViewMoveCursor(sublime_plugin.TextCommand):
@@ -109,10 +115,6 @@ class TerminalViewKeypress(sublime_plugin.TextCommand):
         if type(key) is not str:
             sublime.error_message("Terminal View: Got keypress with non-string key")
             return
-
-        # if alt:
-            # sublime.error_message("Terminal View: Alt key is not supported yet")
-            # return
 
         if meta:
             sublime.error_message("Terminal View: Meta key is not supported yet")
@@ -137,42 +139,80 @@ class TerminalViewKeypress(sublime_plugin.TextCommand):
         cb(key, ctrl, alt, shift, meta)
 
 
-class TerminalViewClear(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.view.erase(edit, sublime.Region(0, self.view.size()))
-
-
 class TerminalViewUpdateLines(sublime_plugin.TextCommand):
     def run(self, edit, lines, display, color_map):
+        clear_time = 0
+        update_time = 0
+        color_time = 0
         self.view.set_read_only(False)
         for line_no in sorted(lines):
-
             # We may get line numbers outside the current display if it was
             # resized to be smaller
-            try:
-                content = display[line_no]
-            except:
-                return
+            if line_no > len(display) - 1:
+                break
 
-            p = self.view.text_point(line_no, 0)
-            line_region = self.view.line(p)
-            if line_region.empty():
-                self.view.replace(edit, line_region, content + "\n")
-            else:
-                self.view.replace(edit, line_region, content)
+            # Clear any colors on the line
+            start = time.time()
+            self._remove_color_regions_on_line(line_no)
+            t = time.time() - start
+            clear_time = clear_time + t
 
-            # Color
+            # Update the line
+            start = time.time()
+            self._update_line_content(edit, line_no, display[line_no])
+            t = time.time() - start
+            update_time = update_time + t
+
+            # Apply colors to the line if there are any on it
             if str(line_no) in color_map:
-                for idx, colors in color_map[str(line_no)].items():
-                    # print(idx)
-                    # print(colors)
-                    # print("")
-                    p = self.view.text_point(line_no, int(idx))
-                    reg = sublime.Region(p, p+1)
-                    key = "%i,%s" % (line_no, idx)
-                    self.view.add_regions(key, [reg], "terminalview.black_green", flags=sublime.DRAW_NO_OUTLINE | sublime.PERSISTENT)
+                start = time.time()
+                self._update_line_colors(line_no, color_map[str(line_no)])
+                t = time.time() - start
+                color_time = color_time + t
 
         self.view.set_read_only(True)
+        print("Done")
+        print(clear_time)
+        print(update_time)
+        print(color_time)
+        print("---")
+
+    def _update_line_content(self, edit, line_no, content):
+        p = self.view.text_point(line_no, 0)
+        line_region = self.view.line(p)
+        if line_region.empty():
+            self.view.replace(edit, line_region, content + "\n")
+        else:
+            self.view.replace(edit, line_region, content)
+
+    def _update_line_colors(self, line_no, line_color_map):
+        for idx, field in line_color_map.items():
+            length = field["field_length"]
+            color_scope = "terminalview.%s_%s" % (field["color"][0], field["color"][1])
+
+            p = self.view.text_point(line_no, int(idx))
+            buffer_region = sublime.Region(p, p+length)
+            region_key = "%i,%s" % (line_no, idx)
+            flags = sublime.DRAW_NO_OUTLINE | sublime.PERSISTENT
+            self.view.add_regions(region_key, [buffer_region], color_scope, flags=flags)
+            self._register_color_region(line_no, region_key)
+
+    def _register_color_region(self, line_no, key):
+        settings = self.view.settings().get("terminal_view_color_regions", {})
+        if str(line_no) in settings:
+            settings[str(line_no)].append(key)
+        else:
+            settings[str(line_no)] = [key]
+
+        self.view.settings().set("terminal_view_color_regions", settings)
+
+
+    def _remove_color_regions_on_line(self, line_no):
+        settings = self.view.settings().get("terminal_view_color_regions", None)
+        if str(line_no) in settings:
+            regions = settings[str(line_no)]
+            for region in regions:
+                self.view.erase_regions(region)
 
 
 class TerminalViewOnCloseCleanup(sublime_plugin.EventListener):
@@ -182,7 +222,7 @@ class TerminalViewOnCloseCleanup(sublime_plugin.EventListener):
             del global_keypress_callbacks[view.id()]
 
 
-def convert_pyte_buffer_to_colormap_for_lines(buffer, lines):
+def convert_pyte_buffer_lines_to_colormap(buffer, lines):
     color_map = {}
     for line_index in lines:
         # There may be lines outside the buffer after terminal was resized.
@@ -190,14 +230,43 @@ def convert_pyte_buffer_to_colormap_for_lines(buffer, lines):
         if line_index > len(buffer) - 1:
             break
 
-        # Note that we have to use strings as key otherwise ST3 cannot send the
-        # data between commands
+        # Get line and process all colors on that. If there are multiple
+        # continuous fields with same color we want to combine them for
+        # optimization and because it looks better when rendered in ST3.
+        last_color = None
+        last_index = None
         line = buffer[line_index]
         for char_index, char in enumerate(line):
+            # If the current char is different than default color
             if char.fg != "default" or char.bg != "default":
+                # If we havent inserted any info about this line in the
+                # color_map yet create the empty dict now
                 if str(line_index) not in color_map:
                     color_map[str(line_index)] = {}
-                color_map[str(line_index)][str(char_index)] = (char.bg, char.fg)
+
+                # Default bg is black
+                if char.bg is "default":
+                    bg = "black"
+                else:
+                    bg = char.bg
+
+                # Default fg is white
+                if char.fg is "default":
+                    fg = "white"
+                else:
+                    fg = char.fg
+
+                # If the color is the same as the previous one we parsed
+                color = (bg, fg)
+                if last_color == color:
+                    length = color_map[str(line_index)][str(last_index)]["field_length"]
+                    color_map[str(line_index)][str(last_index)]["field_length"] = length + 1
+                else:
+                    last_color = color
+                    last_index = char_index
+                    color_map[str(line_index)][str(char_index)] = {"color": color, "field_length": 1}
+            else:
+                last_color = None
 
     return color_map
 
