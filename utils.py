ae195d167db0e2578f96de4613d2d921c3d8d43c
Wramberg <jesper.wramberg@gmail.com>
2017-06-03 22:05:00 +0200

    Add terminal prefix to setting name

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import sublime
 
 def log_to_console(string):
     settings = sublime.load_settings('TerminalView.sublime-settings')
-    print_debug = settings.get("print_debug", False)
+    print_debug = settings.get("terminal_view_print_debug", False)
     if print_debug:
         prefix = "[terminal_view debug] [%.3f] " % (time.time())
         print(prefix + string)
