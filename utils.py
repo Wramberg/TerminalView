31700927421e592a2871af4910c634a50430b989
Randy Lai <randy.cs.lai@gmail.com>
2017-07-27 17:59:07 -0400

    Added a helper function to send command string to shell other plugins (#35)
    * use TerminalViewManager to manage terminal views
    
    Right now, some properties are store is a view object which is not managed by
    TerminalView but [sublime_plugin][1]. This is not satifactory, so I added a
    TerminalViewManager to store the TerminalView instance.
    
    The instance can be retrieved later for later purposes. Ultimately, the
    `view.foo` should be replace by `TerminalViewManager.load_from_id(vid).foo`.
    
    [1]: https://github.com/twolfson/sublime-files/blob/afeed6b8f75ca07ed7f1de80d10a9a2a12764e06/sublime_plugin.py#L196
    
    * added a helper function to send string

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ Some utility functions for the TerminalView plugin
 """
 import time
 import sublime
+import sublime_plugin
 
 
 class ConsoleLogger():
@@ -39,3 +40,32 @@ class TerminalViewManager():
             return cls.terminal_views[vid]
         else:
             raise Exception("terminal view not found.")
+
+
+class TerminalViewSendString(sublime_plugin.WindowCommand):
+    """
+    A command to send any text to the active terminal.
+    Example to send sigint:
+        window.run_command("terminal_view_send_string", args={"string": "\x03"})
+    """
+    def run(self, string, current_window_only=True):
+        if current_window_only:
+            windows = [self.window]
+        else:
+            windows = sublime.windows()
+        view = None
+        for w in windows:
+            for v in w.views():
+                if v.settings().get("terminal_view"):
+                    group, index = w.get_view_index(v)
+                    active_view = w.active_view_in_group(group)
+                    if active_view == v:
+                        view = v
+                        break
+
+        if not view:
+            print("no terminal found.")
+            return
+
+        terminal_view = TerminalViewManager.load_from_id(view.id())
+        terminal_view._shell._send_string(string)
